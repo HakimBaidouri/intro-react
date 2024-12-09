3d68eb7e585b408eeebf76f3c9be81645ce0e1ec
Hakim Baidouri <hakim.baidouri@hotmail.com>
2024-12-09 15:09:43 +0100

    Statue on todo list added

--- src/Checkbox_form.jsx
+++ src/Checkbox_form.jsx
@@ -1,20 +1,43 @@
-const Checkbox_form = () => {
+import React, { useState } from "react";
+
+const CheckboxForm = () => {
+    const initialTodos = [
+        { id: 0, message: "My first todo", statue: "As not done" },
+        { id: 1, message: "My second todo", statue: "As not done" }
+    ];
+
+    const [todos, setTodos] = useState(initialTodos);
+
+    // Fonction pour gérer les changements de checkbox
+    const handleCheckboxChange = (id) => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) =>
+                todo.id === id
+                    ? { ...todo, statue: todo.statue === "As done" ? "As not done" : "As done" }
+                    : todo
+            )
+        );
+    };
+
     return (
-        <div className="Radio-form">
+        <div className="Checkbox-form">
             <h2>Todos</h2>
-            <form action="">
-                <label htmlFor="">
-                    <input type="checkbox" name="Todo" /> 
-                    Learn React
-                </label>
-                <br />
-                <label htmlFor="">
-                    <input type="checkbox" name="Todo" /> 
-                    Be Awesome!
-                </label>
+            <form>
+                {todos.map((todo) => (
+                    <label key={todo.id} htmlFor={`todo-${todo.id}`}>
+                        <input
+                            type="checkbox"
+                            id={`todo-${todo.id}`}
+                            checked={todo.statue === "As done"}
+                            onChange={() => handleCheckboxChange(todo.id)} // Met à jour le statut
+                        />{" "}
+                        {todo.message}
+                        <br />
+                    </label>
+                ))}
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Checkbox_form
+export default CheckboxForm;
